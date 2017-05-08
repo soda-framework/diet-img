4ea5367eca4555ea5cee07a3dffbc9981359a5b1
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-05-08 13:49:26 +0930

    Cache url

--- src/ImageProcessor.php
+++ src/ImageProcessor.php
@@ -2,6 +2,7 @@
 
 namespace Soda\DietImage;
 
+use Illuminate\Support\Facades\Cache;
 use League\Glide\Server;
 
 class ImageProcessor
@@ -14,13 +15,25 @@ class ImageProcessor
         $this->imageServer = $imageServer;
     }
 
-    public function optimize($imgUrl, $manipulations)
+    public function optimize($imgUrl, $manipulations = [], $cache = true)
     {
         // If string is supplied, assume it is a preset
-        if ($manipulations && ! is_array($manipulations)) {
+        if ($manipulations && !is_array($manipulations)) {
             $manipulations = ['p' => $manipulations];
         }
 
-        return $this->imageServer->getBaseUrl() . '/' . $this->imageServer->makeImage($imgUrl, $manipulations);
+        return $cache ? Cache::rememberForever($this->getCacheKey($imgUrl, $manipulations), function () use ($imgUrl, $manipulations) {
+            return $this->generateImage($imgUrl, $manipulations);
+        }) : $this->generateImage($imgUrl, $manipulations);
+    }
+
+    protected function getCacheKey($imgUrl, array $manipulations = [])
+    {
+        return $this->imageServer->getBaseUrl().'/'.$this->imageServer->getCachePath($imgUrl, $manipulations);
+    }
+
+    protected function generateImage($imgUrl, $manipulations = [])
+    {
+        return $this->imageServer->getBaseUrl().'/'.$this->imageServer->makeImage($imgUrl, $manipulations);
     }
 }
